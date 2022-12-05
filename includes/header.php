3042a127c4ca368e03215f3dfc5ccd0cd1069342
qlifford <otienocee@gmail.com>
2022-12-05 08:58:11 +0300

    first commit

---
+++ includes/header.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Daddies Goodies | Home</title>
+    <style>
+      body{
+        background-image: url(images/bg3.jpg) !important;
+        display: d-flex;
+        /* background-repeat: no-repeat; */
+        justify-content: center;
+        align-items: center;
+        height: 100%;
+      }
+    </style>
+
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link type="text/css" rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/alertify.min.css"/>
+    <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/themes/bootstrap.min.css"/>
+
+    <?php include('navbar.php');?>
+
+
